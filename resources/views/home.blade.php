936d9a8960e40e6d9e9d53fb8ab55c6abc3a38c6
salsabilaagustinpy@gmail.com <126636885+SalsabilaAgustinPutriYendi@users.noreply.github.com>
2024-08-29 16:09:09 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Home</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis molestiae, minus ratione quam nisi enim quaerat? At ipsam earum molestias deserunt velit assumenda nulla commodi voluptate tempore reprehenderit soluta, deleniti sunt animi. Placeat provident sint aut vitae nesciunt quis omnis pariatur eius porro, voluptatibus minus facere iure? Quos, praesentium iusto.</p>
 </body>
 </html>
